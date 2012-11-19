fda95bc1ae6e7c91c4c84bdd735388d7e1ede224
JeffJacobson <jeffrey.m.jacobson@gmail.com>
2012-11-19 15:15:17 -0800

    Added files to repository

---
+++ script/default.js
@@ -0,0 +1,18 @@
+﻿/*global require*/
+require(["dojo/on", "esri/map", "esri/layers/agstiled", "esri/dijit/Print"], function (on) {
+	"use strict";
+
+	var map;
+
+	map = new esri.Map("map");
+
+	map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer"));
+	map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer"));
+	map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer("http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer"));
+
+	dojo.connect(map, "onLoad", function () {
+		on(window, "resize", function () {
+			map.resize();
+		});
+	});
+});
